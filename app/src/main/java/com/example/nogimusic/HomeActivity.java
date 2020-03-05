fae386b5f3c56169ab3eaa8b7e5d2bcd32266fed
Asuka <1162498825@qq.com>
2020-03-05 13:44:48 +0800

    6th commit

--- app/src/main/java/com/example/nogimusic/HomeActivity.java
+++ app/src/main/java/com/example/nogimusic/HomeActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,7 +14,12 @@ import android.widget.TextView;
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Music_home music_home_fragment;
+    private Music_recommend music_recommend_fragment;
+    private Social_contact social_contact_fragment;
 
+
+    //底部导航栏监听
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -19,13 +27,16 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    replaceFragment(music_home_fragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    replaceFragment(music_recommend_fragment);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                   // mTextMessage.setText(R.string.title_notifications);
+                    replaceFragment(social_contact_fragment);
                     return true;
             }
             return false;
@@ -40,6 +51,8 @@ public class HomeActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initfragment();
+        replaceFragment(music_home_fragment);
     }
 
     @Override
@@ -50,5 +63,18 @@ public class HomeActivity extends AppCompatActivity {
         intent.addCategory(Intent.CATEGORY_HOME);
         startActivity(intent);
 
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.homepage, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void initfragment(){
+        music_home_fragment = new Music_home();
+        music_recommend_fragment = new Music_recommend();
+        social_contact_fragment = new Social_contact();
     }
 }
---
+++ app/src/main/java/com/example/nogimusic/Music_recommend.java
@@ -0,0 +1,19 @@
+package com.example.nogimusic;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Music_recommend extends Fragment {
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.music_recommend, container, false);
+        return view;
+    }
+}
